0020af8fbe5b616290e083af97a32af6fb721da6
Hann077 <rebecaleon06@iesamachado.org>
2024-12-17 14:28:03 +0100

    change

---
+++ javascript/circulos.js
@@ -0,0 +1,11 @@
+/*
+Este fichero contiene el código para ocultar un circulo
+*/
+function ocultar(id){
+    //Esta función permite ocultar un circulo
+    document.getElementById(id).style.visibility = 'hidden';
+}
+function mostrar(id){
+    //Esta función permite mostrar un circulo
+    document.getElementById(id).style.visibility = 'visible'
+}
